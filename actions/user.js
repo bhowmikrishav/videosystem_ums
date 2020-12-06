9db95b4cb4376e788d3f217858a49bba0b0f7390
Rishav Bhowmik <tgs.rishav.bhowmik@gmail.com>
2020-12-06 16:23:00 +0530

    backend complete

--- actions/user.js
+++ actions/user.js
@@ -1,4 +1,4 @@
-const mongodb = require('mongodb')
+//const mongodb = require('mongodb')
 const {DB} = require('./connect_db')
 const jwt = require('jsonwebtoken')
 const private_manifest = require('../manifest/private.json')
